bc95342be73e8a3b2920692bff58246abdb15a45
ryangalvaogp <60650064+ryangalvaogp@users.noreply.github.com>
2021-06-13 14:16:57 -0300

    Feat: Added Product Sale Controllers and function book and product Sale remove routes

---
+++ src/types/productSalesControllersType.ts
@@ -0,0 +1,21 @@
+export type detailsPurchaseProps = {
+    amount: number
+    offeredPrice: number
+    soldPrice: number
+    factoryPrice: number
+    paymentMethod: string
+}
+
+export type valuesProductSale = {
+    idSale: string
+    idClient: string | string[]
+    idEmployee: string | string[]
+    idProduct: string
+    amount: number
+    offeredPrice: number
+    soldPrice: number
+    factoryPrice: number
+    paymentMethod: string
+    saleDate: string
+    
+}
--- src/utils/validateSale.ts
+++ src/utils/validateSale.ts
@@ -1,5 +1,7 @@
 import { connection } from "../database/connection"
 import { valuesBookSale } from "../types/bookSalesControllersTypes"
+import { valuesProductSale } from "../types/productSalesControllersType";
+import { ProductStockCrud } from "../types/productStockControllersTypes";
 import { boockStockProps } from "../types/stockBoockControllersTypes";
 
 interface returnMessageProps {
@@ -9,40 +11,56 @@ interface returnMessageProps {
     currentAmount?: number
 }
 
-export const validateSale = async (values: valuesBookSale) => {
-    return new Promise<returnMessageProps>(async (resolve, reject) => {
-        const bookStock = await connection<boockStockProps>('bookStock')
-        .where('idBook', values.idBook)
-        .select('*')
-        .first();
+type keyValidationProps = 'books' | 'product'
 
+export const validateSale = async (key: keyValidationProps, values: valuesBookSale | valuesProductSale) => {
+    return new Promise<returnMessageProps>(async (resolve, reject) => {
+
+        let stock;
         let returnMessage: returnMessageProps;
 
-        if (bookStock.amount === 0) {
+        switch (key) {
+            case 'books':
+                stock = await connection<boockStockProps>('bookStock')//@ts-expect-error
+                    .where('idBook', values.idBook)
+                    .select('*')
+                    .first();
+                break;
+
+            case 'product':
+                stock = await connection<ProductStockCrud>('productsStock')//@ts-expect-error
+                    .where('idProduct', values.idProduct)
+                    .select('*')
+                    .first();
+                break;
+        }
+
+
+        if (stock.amount === 0) {
             returnMessage = {
                 isValidate: false,
-                motive: 'Insufficient amount of books in stock',
-                resolution: 'Add books to stock'
+                motive: `Insufficient amount of ${key} in stock`,
+                resolution: `Add ${key} to stock`,
             }
             reject(returnMessage);
             return returnMessage
         };
 
-        if (values.amount > bookStock.amount) {
+        if (values.amount > stock.amount) {
             returnMessage = {
                 isValidate: false,
-                motive: 'Insufficient amount of books in stock',
-                resolution: 'Add books to stock',
+                motive: `Insufficient amount of ${key} in stock`,
+                resolution: `Add ${key} to stock`,
             }
             reject(returnMessage);
             return returnMessage;
         }
 
-        if (values.soldPrice < bookStock.factoryPrice) {
+        if (values.soldPrice < stock.factoryPrice) {
             returnMessage = {
                 isValidate: false,
                 motive: 'Price cannot be sold below factory price',
-                resolution: 'Increase book price'
+                resolution: `Increase ${key} price`
             }
             reject(returnMessage);
             return returnMessage;
@@ -50,7 +68,7 @@ export const validateSale = async (values: valuesBookSale) => {
 
         returnMessage = {
             isValidate: true,
-            currentAmount: bookStock.amount
+            currentAmount: stock.amount
         }
         resolve(returnMessage);
         return returnMessage;
