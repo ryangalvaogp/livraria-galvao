5d1166827d656a607b34a70ba25ee41036ea04f3
ryangalvaogp <60650064+ryangalvaogp@users.noreply.github.com>
2021-06-16 15:59:53 -0300

    Feat: Added achievements controllers and your functions

---
+++ src/controllers/achievement/achievementsControllers.ts
@@ -0,0 +1,113 @@
+import { Response, Request } from 'express';
+import { connection } from '../../database/connection';
+import { AchievementProps } from '../../types/achievementControllerstTypes';
+import crypto from 'crypto';
+import { useCSV } from '../../utils/useCSV';
+
+export default {
+    async index(req: Request, res: Response) {
+        try {
+            const allAchievements = await connection<AchievementProps>('achievement').select('*');
+
+            return res.json(allAchievements);
+        } catch (error) {
+            return res.json({
+                status: `Error showing all achievements`,
+                error,
+            });
+        };
+    },
+
+    async create(req: Request, res: Response) {
+        const id = crypto.randomBytes(4).toString('hex');
+        const values = req.body;
+        const csv = req.file;
+
+        let valuesAchievement: AchievementProps[] | AchievementProps
+
+        try {
+            if (!req.file || (req.file && req.body)) {
+                valuesAchievement = {
+                    id,
+                    title: values.title,
+                    type: values.type,
+                    reward: values.reward,
+                    amount: values.amount,
+                    isActive: values.isActive,
+                };
+
+                await connection('achievement').insert(valuesAchievement);
+            };
+
+            if (req.file && !req.body) {
+                valuesAchievement = await useCSV(csv.path);
+                for (var i = 0; i < valuesAchievement.length; i++) {
+                    console.log(valuesAchievement[i])
+                    await connection('achievement').insert(valuesAchievement[i]);
+                };
+            };
+
+            return res.json({ status: `The achievement has been successfully registered` });
+        } catch (error) {
+            return res.json({
+                status: "Error registering the achievement",
+                error,
+            });
+        };
+    },
+
+    async showOne(req: Request, res: Response) {
+        const { id: idAchievement } = req.params;
+
+        try {
+            const achievement = await connection('achievement')
+                .select('*')
+                .where('id', idAchievement)
+                .first();
+
+            return res.json(achievement);
+        } catch (error) {
+            return res.json({
+                status: `Error showing achievement`,
+                error,
+            });
+        };
+    },
+
+    async Modify(req: Request, res: Response) {
+        const { id: idAchievement } = req.params;
+        const newValues = req.body;
+
+        try {
+            await connection('achievement')
+                .where('id', idAchievement)
+                .update(newValues);
+
+            return res.json({
+                status: 'The achievement has been successfully modified'
+            });
+        } catch (error) {
+            return res.json({
+                status: 'Error modifying achievement',
+                error,
+            });
+        };
+    },
+
+    async Delete(req: Request, res: Response) {
+        const { id: idAchievement } = req.params;
+
+        try {
+            await connection('achievement')
+                .where('id', idAchievement)
+                .delete();
+
+            return res.json({ status: `Successfully deleted achievement` });
+        } catch (error) {
+            return res.json({
+                status: `Error deleting achievement`,
+                error,
+            });
+        };
+    },
+};
---
+++ src/controllers/achievement/userAchievementsControllers.ts
@@ -0,0 +1,172 @@
+import { Response, Request } from 'express';
+import { connection } from '../../database/connection';
+import { UserAchievementsProps } from '../../types/userAchievementControllersTypes';
+import { convertDateToPrint, getDateNow } from '../../utils/date';
+import crypto from 'crypto';
+
+export default {
+    async index(req: Request, res: Response) {
+        try {
+            let allUserAchievements = await connection<UserAchievementsProps>
+                ('userAchievements')
+                .innerJoin('user', 'user.id', 'userAchievements.idUser')
+                .innerJoin('achievement', 'achievement.id', 'userAchievements.idAchievement')
+                .select(
+                    'userAchievements.id', 'idUser', 'idAchievement', 'achievementDate',
+                    'isCollected', 'name', 'email', 'cpf', 'cell', 'permission',
+                    'premium', 'title', 'type', 'reward', 'amount', 'isActive', 'collectedDate'
+                );
+            allUserAchievements = allUserAchievements.map(userAchievements => {
+                return {
+                    idUserAchievemente: userAchievements.id,
+                    idAchievement: userAchievements.idAchievement,
+                    achievementDate: convertDateToPrint(userAchievements.achievementDate),
+                    isCollected: userAchievements.isCollected,
+                    collectedDate: userAchievements.collectedDate,
+                    title: userAchievements.title,
+                    type: userAchievements.type,
+                    reward: userAchievements.reward,
+                    amount: userAchievements.amount,
+                    isActive: userAchievements.isActive,
+                    user: {
+                        idUser: userAchievements.idUser,
+                        name: userAchievements.name,
+                        email: userAchievements.email,
+                        cpf: userAchievements.cpf,
+                        cell: userAchievements.cell,
+                        permission: userAchievements.permission,
+                        premium: userAchievements.premium,
+                    },
+                };
+            });
+
+            return res.json(allUserAchievements);
+        } catch (error) {
+            return res.json({
+                status: `Error showing all user achievements`,
+                error,
+            });
+        };
+    },
+
+    async create(req: Request, res: Response) {
+        const id = crypto.randomBytes(4).toString('hex');
+        const values = req.body;
+        values.idAchievement = req.headers.idachievement;
+        values.idUser = req.headers.iduser;
+
+        try {
+
+            const valuesUserAchievement: UserAchievementsProps = {
+                id,
+                idUser: values.idUser,
+                idAchievement: values.idAchievement,
+                achievementDate: getDateNow(),
+                isCollected: values.isCollected || false,
+                collectedDate: values.isCollected === true ? getDateNow() : ''
+            };
+            await connection<UserAchievementsProps>
+                ('userAchievements')
+                .insert(valuesUserAchievement);
+
+            return res.json({
+                status: `The user achievement has been successfully registered`,
+            });
+        } catch (error) {
+            return res.json({
+                status: "Error registering the user achievement",
+                error,
+            });
+        };
+    },
+
+    async showOne(req: Request, res: Response) {
+        const { id: idUser } = req.params;
+
+        try {
+            let userAchievements = await connection<UserAchievementsProps>
+                ('userAchievements')
+                .innerJoin('user', 'user.id', 'userAchievements.idUser')
+                .innerJoin('achievement', 'achievement.id', 'userAchievements.idAchievement')
+                .where('idUser', idUser)
+                .select(
+                    'userAchievements.id', 'idUser', 'idAchievement', 'achievementDate',
+                    'isCollected', 'name', 'email', 'cpf', 'cell', 'permission',
+                    'premium', 'title', 'type', 'reward', 'amount', 'isActive',
+                    'collectedDate',
+                );
+
+            userAchievements = userAchievements.map(userAchievements => {
+                return {
+                    idUserAchievemente: userAchievements.id,
+                    idAchievement: userAchievements.idAchievement,
+                    achievementDate: convertDateToPrint(userAchievements.achievementDate),
+                    isCollected: userAchievements.isCollected,
+                    collectedDate: userAchievements.collectedDate,
+                    title: userAchievements.title,
+                    type: userAchievements.type,
+                    reward: userAchievements.reward,
+                    amount: userAchievements.amount,
+                    isActive: userAchievements.isActive,
+                    user: {
+                        idUser: userAchievements.idUser,
+                        name: userAchievements.name,
+                        email: userAchievements.email,
+                        cpf: userAchievements.cpf,
+                        cell: userAchievements.cell,
+                        permission: userAchievements.permission,
+                        premium: userAchievements.premium,
+                    },
+                };
+            });
+
+            return res.json(userAchievements);
+        } catch (error) {
+            return res.json({
+                status: `Error showing user achievement`,
+                error,
+            });
+        };
+    },
+
+    async Modify(req: Request, res: Response) {
+        const { id: idUserAchievement } = req.params;
+        const newValues = req.body;
+        newValues.collectedDate = getDateNow();
+
+        try {
+            await connection<UserAchievementsProps>
+                ('userAchievements')
+                .where('id', idUserAchievement)
+                .update(newValues);
+
+            return res.json({
+                status: 'The user achievement has been successfully modified',
+            });
+        } catch (error) {
+            return res.json({
+                status: 'Error modifying user achievement',
+                error,
+            });
+        };
+    },
+
+    async Delete(req: Request, res: Response) {
+        const { id: idUserAchievement } = req.params;
+
+        try {
+            await connection<UserAchievementsProps>('userAchievements')
+                .where('id', idUserAchievement)
+                .delete();
+
+            return res.json({
+                status: `Successfully deleted user achievement`,
+            });
+        } catch (error) {
+            return res.json({
+                status: `Error deleting user achievement`,
+                error,
+            });
+        };
+    },
+};
