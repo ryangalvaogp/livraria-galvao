c566b373d0c5737fb1ceb3316bbd7b32245d1526
ryangalvaogp <60650064+ryangalvaogp@users.noreply.github.com>
2021-09-08 14:17:58 -0300

    feat: Added Cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import {route} from './routes';
+import { route } from './routes';
+import cors from 'cors';
 
 const server = express();
 
 server.use(express.json());
+server.use(cors());
 server.use(route);
-
 
 server.listen(process.env.PORT || 3333);
